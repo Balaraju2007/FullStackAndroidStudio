d63bb8e60c2330f3df4f53ba535ba07943eda135
balaraju <balarajumukalla3@gmail.com>
2026-02-27 00:32:55 +0530

    package.json issues

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,4 @@ connectDB();
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server runningg on port ${PORT}`));
